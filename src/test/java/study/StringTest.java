c145022931079ef7145b9ccb01aa3d50d5181ce6
janghomoon <sonyjangho@gmail.com>
2022-10-01 00:46:05 +0900

    refactor : 학습테스트 string Test  요구사항 1

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -11,17 +11,21 @@ public class StringTest {
   @Test
   @DisplayName("요구사항 1 - 1,2  ,로 split 분리 테스트")
   void arraySplitTest() {
-    assertThat("1,2".split(",")).contains("1", "2");
 
+    String[] values = "1,2".split(",");
+    assertThat(values).contains("1", "2");
+    assertThat(values).containsExactly("1", "2");
   }
 
 
   @Test
   @DisplayName("요구사항 1 - 1  ,로 split 분리 테스트")
   void StringSplitTest() {
-    assertThat("1".split(",")).contains("1");
-  }
 
+    String[] values = "1".split(",");
+    assertThat(values).contains("1");
+    assertThat(values).containsExactly("1");
+  }
 
 
 }
