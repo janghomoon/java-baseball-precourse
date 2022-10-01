aa9b608f92f9a0dec676564ccff06c8761f21da1
janghomoon <sonyjangho@gmail.com>
2022-10-02 00:56:22 +0900

    feat : 입력값 valid 추가

---
+++ src/main/java/util/ValidationUtil.java
@@ -0,0 +1,27 @@
+package util;
+
+import java.util.HashSet;
+import java.util.List;
+
+public class ValidationUtil {
+
+  final static int MIN_VALUE = 0;
+  final static int MAX_VALUE = 10;
+
+  final static int MAX_LENGTH = 3;
+  public static boolean isNumber(int value) {
+
+    return MIN_VALUE < value && value < MAX_VALUE;
+  }
+
+  public static boolean isLength(List<Integer> valueList) {
+
+    return valueList.size() == MAX_LENGTH;
+  }
+
+
+  public static boolean isDuplication(List<Integer> valueList) {
+
+    return new HashSet<>(valueList).size() == valueList.size();
+  }
+}
