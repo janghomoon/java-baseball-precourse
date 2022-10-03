a78f2b588da987bc7d41db4ac98a8fae3503b87b
janghomoon <sonyjangho@gmail.com>
2022-10-03 19:08:15 +0900

    feat : controller 추가

---
+++ src/main/java/baseball/controller/BaseBallController.java
@@ -0,0 +1,45 @@
+package baseball.controller;
+
+import baseball.service.BaseBallService;
+import baseball.util.MessageUtil;
+import camp.nextstep.edu.missionutils.Console;
+
+public class BaseBallController {
+
+  BaseBallService baseBallService = new BaseBallService();
+
+  final Integer RE_GAME = 1;
+  final Integer END_GAME = 2;
+
+  public void run() throws IllegalArgumentException {
+    startGame();
+    endGame();
+    reGame();
+  }
+
+  private void startGame() {
+//    baseBallService.play();
+
+  }
+
+  private void endGame() {
+    MessageUtil.endMsg();
+  }
+
+  private void reGame() {
+    MessageUtil.retryMsg();
+    if (getInputNum() == RE_GAME) {
+      run();
+    }
+  };
+
+  private int getInputNum() throws IllegalArgumentException {
+    int inputNum = Integer.parseInt(Console.readLine());
+
+    if (inputNum == 0 || inputNum > END_GAME) {
+      throw new IllegalArgumentException();
+    }
+    return inputNum;
+  }
+
+}
